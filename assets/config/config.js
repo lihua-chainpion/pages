f88c8c25882a85375eb71d15710b489d777919b1
lihuabing <2946632291@qq.com>
2022-03-14 11:49:26 +0800

    change network to goerli

--- assets/config/config.js
+++ assets/config/config.js
@@ -1,14 +1,14 @@
 function getConfig() {
   return {
-    chainId: '0x3',
-    chainName: 'Ropsten Test Network',
+    chainId: '0x5',
+    chainName: 'Goerli Test Network',
     ittcContract: {
-      address: '0x518955b9346ee238e2c0dbad7021c7e59b355485',
+      address: '0xcfe651d914415a0029fe3e4233ef07e51cb0eb36',
       abi: getIttcAbi(),
       inviteBaseUrl: 'https://lihua-chainpion.github.io/pages/index.html?inviter=',
     },
     usdtContract: {
-      address: '0xd014cd2dea75a1e989be84af0291ddf7e427f64d',
+      address: '0xc715e8213449a5c3c401b520c52a9ca286792f1b',
       abi: getUsdtAbi(),
     },
   }
